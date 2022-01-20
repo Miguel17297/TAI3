f396b283d85366b5f051e2ae38b9d0553630981e
Miguel17297 <almeida.m@ua.pt>
2022-01-20 11:59:46 +0000

    find music

--- src/utils.py
+++ src/utils.py
@@ -6,17 +6,28 @@ ROOT = os.getcwd()
 DB_PATH = os.path.join(ROOT, "dataset")
 BIN_PATH = os.path.join(ROOT, "bin")
 
+
 # Todo: redirecionar o resultado para uma variavel ou então ler o ficheiro
 
 def applyGetMaxFreqs(filepath):
-    if platform.system().lower() == 'windows':
-        f_path = os.path.join(BIN_PATH, "windows", "GetMaxFreqs")
-        return subprocess.run([f_path, "-w", f"./dataset/{filepath[0:-4]}.freqs", "./dataset/" + filepath])
+    assert os.path.exists(filepath), f'Invalid file: {filepath}'
 
-    else:
-        # TODOSomeone check if it works for Linux/Mac
-        f_path = os.path.join(BIN_PATH, "mac", "GetMaxFreqs")
-        return subprocess.run([f_path, "-w", f"./dataset/{filepath[0:-4]}.freqs", "./dataset/" + filepath])
+    f_name, f_format = os.path.splitext(filepath)
+
+    freq_file = f"{f_name}.freqs"
+
+    if f_format != ".freqs":  # if already a .freqs file dont need to recompute
+
+        if platform.system().lower() == 'windows':
+            f_path = os.path.join(BIN_PATH, "windows", "GetMaxFreqs")
+            subprocess.run([f_path, "-w", freq_file, filepath])
+
+        else:
+            f_path = os.path.join(BIN_PATH, "mac", "GetMaxFreqs")
+            subprocess.run([f_path, "-w", freq_file, filepath])
+
+    with open(freq_file, "rb") as f:
+        return f.read()
 
 
 def load_db():
@@ -30,4 +41,3 @@ def load_db():
             db[music_name] = f.read()
 
     return db
-
