b838fff3c5a6f98a1ac82d696c7677f8128c905a
pmasilva20 <pmasilva20@ua.pt>
2022-01-28 19:36:27 +0000

    Tests for noise

--- src/tests.py
+++ src/tests.py
@@ -1,9 +1,13 @@
 import os
 
+import numpy as np
+
 from src.comp_type import CompType
 from src.findmusic import FindMusic
 
 import matplotlib.pyplot as plt
+
+from src.utils import add_noise
 
 SAMPLES_PATH = ''
 
@@ -12,10 +16,13 @@ def read_samples(filepath):
     return [os.path.join(filepath, filename) for filename in os.listdir(filepath)]
 
 
-def get_accuracy(f, samples):
+def get_accuracy(f, samples, noise=None, noise_type=None):
     acc = 0
     for sample in samples:
         actual_song = sample[15:-13]
+        if noise is not None:
+            sample = add_noise(sample, noise, noise_type)
+        print(noise)
         song_found = f.find(sample)
         print(f'Compare "{actual_song}" to "{song_found}"')
         if actual_song in song_found:
@@ -25,7 +32,7 @@ def get_accuracy(f, samples):
 
 def stats_compressors(samples_10):
     compressor_acc = []
-    compressors = ['gzip', 'lzma']
+    compressors = ['gzip', 'lzma', 'bzip2']
     for compressor in compressors:
         f = FindMusic(compressor)
         acc = get_accuracy(f, samples_10)
@@ -45,7 +52,7 @@ def stats_compressors(samples_10):
     plt.show()
 
 
-def stats_sample_size(samples_10, samples_20, samples_30,compressor):
+def stats_sample_size(samples_10, samples_20, samples_30, compressor):
     samples_acc = []
     for samples in [samples_10, samples_20, samples_30]:
         f = FindMusic(compressor)
@@ -59,6 +66,21 @@ def stats_sample_size(samples_10, samples_20, samples_30,compressor):
     plt.show()
 
 
+def stats_noise(samples_20, compressor, noise_type):
+    samples_acc = []
+
+    for noise in [-0.4, -0.3, -0.2, -0.1, -0.05, 0, 0.05, 0.1, 0.2, 0.3, 0.4]:
+        f = FindMusic(compressor)
+        acc = get_accuracy(f, samples_20, noise, noise_type)
+        samples_acc.append(acc * 100)
+
+    plt.plot([-0.4, -0.3, -0.2, -0.1, -0.05, 0, 0.05, 0.1, 0.2, 0.3, 0.4], samples_acc)
+    plt.xlabel("Noise Level")
+    plt.ylabel("Accuracy(%)")
+    plt.title(f"Accuracy by Noise Level for {compressor.upper()} Compressor (using {noise_type[0].upper() + noise_type[1:]})")
+    plt.show()
+
+
 def main():
     samples = read_samples('..\samples_raw')
     samples_10 = [sample for sample in samples if '10' in sample[-6:]]
@@ -68,8 +90,9 @@ def main():
     stats_compressors(samples_20)
     stats_sample_size(samples_10, samples_20, samples_30, 'lzma')
     stats_sample_size(samples_10, samples_20, samples_30, 'gzip')
-
-
+    stats_sample_size(samples_10, samples_20, samples_30, 'bzip2')
+    stats_noise(samples_30, 'bzip2', 'whitenoise')
+    stats_noise(samples_30, 'bzip2', 'brownnoise')
 
 
 if __name__ == '__main__':
